ede3c9fd865189c5e04e7e9ec427954ed3220e76
Martin <Mar.camposa@duocuc.cl>
2024-08-30 16:31:22 -0400

    Agregar crud

--- src/app/pages/agregar/agregar.page.ts
+++ src/app/pages/agregar/agregar.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-agregar',
@@ -6,10 +8,74 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./agregar.page.scss'],
 })
 export class AgregarPage implements OnInit {
-
-  constructor() { }
+  id: string = "";
+  nombre: string = "";
+  descripcion: string = "";
+  imagen: string = "";
+  precio!: number;
+  constructor(private router: Router,
+    private toastController: ToastController,
+    private alertController: AlertController) { }
 
   ngOnInit() {
+  }
+
+  //sirve para hacer que aprezca el boton de archivo de imagen en el imput
+
+
+  Agregar() {
+    if (!this.nombre || !this.descripcion || !this.imagen || !this.precio) {
+      this.presentAlert('Campos incompletos', 'Por favor, complete todos los campos.');
+      return;
+    }
+    const nombreRegex = /^[a-zA-ZÀ-ÿ\s-]+$/;
+    if (!nombreRegex.test(this.nombre)) {
+      this.presentAlert('Nombre inválido', 'El nombre solo debe contener letras, espacios y guiones.');
+      return;
+    }
+    if (!nombreRegex.test(this.descripcion)) {
+      this.presentAlert('Descripción inválida', 'La descripción solo debe contener letras, espacios y guiones.');
+      return;
+    }
+
+    const numeroStr = this.precio.toString();
+    if (isNaN(Number(this.precio)) || numeroStr.length > 5 ) {
+      this.presentAlert('Número inválido', 'Debe ingresar un precio apropiado y menor a los 5 digitos o igual .');
+      return;
+    };
+
+
+
+    this.presentToast('top');
+    this.router.navigate(['/menu-crud']);
+
+  };
+
+
+
+
+
+  //PresentAlert
+  async presentAlert(titulo: string, msj: string) {
+    const alert = await this.alertController.create({
+      header: titulo,
+      message: msj,
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+  }
+  //Alerta Toast
+  async presentToast(position: 'top' | 'middle' | 'bottom') {
+    const toast = await this.toastController.create({
+      message: 'Usuario registrado con exito',
+      duration: 2500,
+      position: position,
+      color: 'success'
+
+    });
+
+    await toast.present();
   }
 
 }
