adfa8d28ca0b005bda6da89ab0e8116cda094233
Martin <Mar.camposa@duocuc.cl>
2024-08-30 16:37:53 -0400

    Cupones

--- src/app/pages/cupones/cupones.page.ts
+++ src/app/pages/cupones/cupones.page.ts
@@ -10,6 +10,8 @@ import { AlertController } from '@ionic/angular';
 export class CuponesPage implements OnInit {
   cupon: string = "";
   cuponValido: string = "DESCUENTO50";  
+  cuponValido2: string = "DESCUENTO10"; 
+  cuponValido3: string = "DESCUENTO30"; 
 
   constructor(private router: Router, 
     private alertController: AlertController) { }
@@ -17,13 +19,14 @@ export class CuponesPage implements OnInit {
   ngOnInit() {}
 
   Canjearcupon() {
-    if (this.cupon === this.cuponValido) {
+    if (this.cupon === this.cuponValido ||this.cupon === this.cuponValido2|| this.cupon === this.cuponValido3 ) {
       // Si el cupón es válido, muestra un mensaje de éxito
       this.presentAlert('¡Cupón válido!', 'El cupón ha sido aplicado correctamente. ¡Disfruta de tu descuento!');
     } else {
       // Si el cupón es incorrecto, muestra un mensaje de error
       this.presentAlert('Cupón no válido', 'El cupón que has ingresado no es válido. Por favor, verifica el código e intenta de nuevo.');
     }
+    
   }
 
   async presentAlert(titulo: string, msj: string) {
